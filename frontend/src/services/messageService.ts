a4f5dc8a23bf0e75c7890b89dcd835a466fdcc87
Bernd Prager <bernd@prager.ws>
2023-12-14 17:26:40 -0800

    replace logos

---
+++ frontend/src/services/messageService.ts
@@ -0,0 +1,37 @@
+// Path: frontend/src/services/messageService.ts
+import config from "../config.json";
+import { getAuthToken } from "./cognitoService"; // Adjust the import based on your actual cognito service
+
+interface MessageData {
+  sessionId: string;
+  timeStamp: string;
+  language: string;
+  userName: string;
+  message: string;
+}
+
+const sendMessage = async (messageData: MessageData): Promise<void> => {
+  const apiEndpoint = config.API_URL + config.MethodPath;
+  const authToken = await getAuthToken();
+
+  try {
+    const response = await fetch(apiEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+        // Include other headers as required
+      },
+      body: JSON.stringify(messageData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to send message");
+    }
+    console.log("Message sent successfully");
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+};
+
+export default sendMessage;
